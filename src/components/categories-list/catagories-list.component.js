6c5460171b3f3b3b6acb949eb3c5045db0fce234
wesley varrasso <wesley@rosalind.bio>
2022-04-13 08:20:44 -0700

    init

---
+++ src/components/categories-list/catagories-list.component.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function CatagoriesListComponent({category}) {
+  const {id, title, imageUrl} = category;
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{backgroundImage: `url(${imageUrl}`}}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop now</p>
+      </div>
+    </div>
+  );
+}
+
+export default CatagoriesListComponent;
