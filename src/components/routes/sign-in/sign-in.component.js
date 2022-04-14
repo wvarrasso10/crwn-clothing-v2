44d6c68387bb3d98a8d72abd117cc16fe46c0a34
wesley varrasso <wesley@rosalind.bio>
2022-04-14 11:09:18 -0700

    firebase sign in and utils

--- src/components/routes/sign-in/sign-in.component.js
+++ src/components/routes/sign-in/sign-in.component.js
@@ -1,9 +1,10 @@
 import React from "react";
-import {signInWithGooglePopup} from "../../../utils/firebase.utils";
+import {signInWithGooglePopup,} from "../../../utils/firebase.utils";
 import GoogleSignInButton from "../../../assets/btn_google_signin_light_normal_web.png";
 
 function SignInComponent() {
   const logGoogleUser = async () => {
+    console.log("here");
     const response = await signInWithGooglePopup();
     console.log(response);
   };
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
-import {getAuth, GoogleAuthProvider,} from "firebase/auth";
-import {getAnalytics} from "firebase/analytics";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import {doc, getDoc, getFirestore, setDoc} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,12 +19,34 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
   prompt: "select_account",
 });
 export const auth = getAuth();
-export const signInWithGooglePopup = () =>
-  signInWithGooglePopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+export const createUserDocFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  //check if user data dpes not exists
+  //create / set the document with the data from userAuth om my collection
+
+  //if user data does exist
+};
